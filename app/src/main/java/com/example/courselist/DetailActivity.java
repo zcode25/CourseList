da8fd602c534e6e862d762d9d80860ea0f72fbf3
zcode25 <adamzein345@gmail.com>
2021-12-30 22:20:49 +0700

    Update Fitur

--- app/src/main/java/com/example/courselist/DetailActivity.java
+++ app/src/main/java/com/example/courselist/DetailActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.courselist.helper.Helper;
+
 public class DetailActivity extends AppCompatActivity {
 
     private TextView nim, name, email, phone;
     private String nimString, nameString, emailString, phoneString;
+    Helper db = new Helper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +34,13 @@ public class DetailActivity extends AppCompatActivity {
         name.setText(nameString);
         email.setText(emailString);
         phone.setText(phoneString);
+
+        TextView subject = findViewById(R.id.text_subject);
+        String count = db.count();
+        subject.setText(count);
+
+        TextView student = findViewById(R.id.text_student);
+        String count2 = db.count2();
+        student.setText(count2);
     }
 }
